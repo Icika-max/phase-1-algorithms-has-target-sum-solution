377a046d652a608c7326d433c160cd25e51c490c
icika-max <icika-max>
2022-12-09 07:10:07 +0300

    completed asignment

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers ={};
+  for (let i=0; i< array.length; i++){
+    const complement = target-array[i]
+    if (seenNumbers[complement])return true
+  seenNumbers[array[i]]=true
+  }
+  return false
 }
-
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
+/*
+Write the Big O time complexity of your function here
 */
 
 /*
-  Add written explanation of your solution here
+Add your pseudocode here
+*/
+
+/*
+Add written explanation of your solution here
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+// add your own custom tests in here
+console.log("Expecting: true");
+console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+console.log("");
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+console.log("Expecting: true");
+console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+console.log("Expecting: false");
+console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
 module.exports = hasTargetSum;
